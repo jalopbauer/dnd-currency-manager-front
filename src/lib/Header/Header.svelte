52ba620e0fb96d70a554a207d9cda5a1d0173e20
jalopbauer <jorge.lopezbauer@ing.austral.edu.ar>
2024-01-16 20:05:29 -0300

    Header

---
+++ src/lib/Header/Header.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    export let headerText : string;
+</script>
+
+<h1 class="header">{headerText}</h1>
+
+<style>
+    .header {
+        padding: 0 16px 0 16px;
+        display: flex;
+        align-items: center;
+        align-self: stretch;
+        background-color: var(--primary-600);
+    }
+</style>
