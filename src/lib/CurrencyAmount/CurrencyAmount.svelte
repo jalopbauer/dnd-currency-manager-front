c44093c93420c59db60a5db0e636892f6f94376c
jalopbauer <jorge.lopezbauer@ing.austral.edu.ar>
2024-01-16 20:20:41 -0300

    CurrencyAmount

---
+++ src/lib/CurrencyAmount/CurrencyAmount.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import Currency from "$lib/Currency/Currency.svelte";
+
+    export let variant : "platinum" | "gold" | "silver" | "bronze";
+    export let amountOfMoney : Number;
+</script>
+
+<div class="currency-amount">
+    <h4>{amountOfMoney}</h4>
+    <Currency variant={variant}/>
+</div>
+
+<style>
+
+    .currency-amount {
+        display: flex;
+        align-items: center;
+        gap: 2px;
+    }
+
+</style>
