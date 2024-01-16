7ae4626dd67595c57f4dbbbc4a35a749ba97a621
jalopbauer <jorge.lopezbauer@ing.austral.edu.ar>
2024-01-16 20:29:00 -0300

    WalletMoney

---
+++ src/lib/WalletMoney/WalletMoney.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import CurrencyAmount from "$lib/CurrencyAmount/CurrencyAmount.svelte";
+    export let amountOfPlatinum : Number;
+    export let amountOfGold : Number;
+    export let amountOfSilver : Number;
+    export let amountOfBronze : Number;
+</script>
+
+<div class="wallet-money">
+    <CurrencyAmount amountOfMoney={amountOfPlatinum} variant="platinum"/>
+    <CurrencyAmount amountOfMoney={amountOfGold} variant="gold"/>
+    <CurrencyAmount amountOfMoney={amountOfSilver} variant="silver"/>
+    <CurrencyAmount amountOfMoney={amountOfBronze}  variant="bronze"/>
+</div>
+
+<style>
+
+    .wallet-money {
+        display: flex;
+        padding: 8px;
+        justify-content: space-between;
+        align-items: flex-start;
+        align-self: stretch;
+    }
+
+</style>
