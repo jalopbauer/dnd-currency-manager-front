e91a517e6b85097d99db01854ddd9a05377111b5
jalopbauer <jorge.lopezbauer@ing.austral.edu.ar>
2024-01-16 20:20:34 -0300

    Currency

---
+++ src/lib/Currency/Currency.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    export let variant : "platinum" | "gold" | "silver" | "bronze";
+</script>
+
+{#if variant === "platinum"}
+    <h4>P</h4>
+{:else if variant === "gold"}
+    <h4>G</h4>
+{:else if variant === "silver"}
+    <h4>S</h4>
+{:else}
+    <h4>B</h4>
+{/if}
